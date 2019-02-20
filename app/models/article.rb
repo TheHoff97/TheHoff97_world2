673aa2f68bc88dc5ec898b2eb6c37244738997f7
Lera <vneviado@andrew.cmu.edu>
2019-02-19 21:52:04 -0500

    Merged articles and controllers

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ApplicationRecord
-	has_one :category
+	has_many :categories
 	
 	validates_presence_of :title
 	validates_presence_of :content
 
-	scope :active, 			 -> { where('active = ?', true) }
-  	scope :alphabetical, -> { order('name') }
+	scope :active, 		 -> { where('active = ?', true) }
+  	scope :alphabetical, -> { order('title') }
 end
